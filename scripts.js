d6ee336803e12f132c09e6423bef050699b92e79
ericknathan <erick.capito@hotmail.com>
2021-09-01 10:54:38 -0300

    🐛 Fixed automatic interval bug

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 "use strict"
 const getId = (id) => document.getElementById(id);
 const newListener = (elementId, eventName, action) => getId(elementId).addEventListener(eventName, action);
-const semaphore = document.getElementById('semaphore');
 let automatic;
 
 newListener('red', 'click', () => semaphorePhases.red())
@@ -12,20 +11,29 @@ newListener('off', 'click', () => semaphorePhases.off())
 
 const semaphorePhases = {
     semaphore: getId('semaphore'),
-    red: () => this.semaphore.src = "./public/images/vermelho.png",
-    yellow: () => this.semaphore.src = "./public/images/amarelo.png",
-    green: () => this.semaphore.src = "./public/images/verde.png",
-    off: () => {
-        this.semaphore.src = "./public/images/desligado.png";
+    red: () => {
         clearInterval(automatic);
+        this.semaphore.src = "./public/images/vermelho.png";
+    },
+    yellow: () => {
+        clearInterval(automatic);
+        this.semaphore.src = "./public/images/amarelo.png";
+    },
+    green: () => {
+        clearInterval(automatic);
+        this.semaphore.src = "./public/images/verde.png";
+    },
+    off: () => {
+        clearInterval(automatic);
+        this.semaphore.src = "./public/images/desligado.png";
     },
     automatic: () => {
+        clearInterval(automatic);
         semaphorePhases.red();
         automatic = setInterval(() => {
             if(this.semaphore.src.includes("verde")) semaphorePhases.red();
             else if(this.semaphore.src.includes("vermelho")) semaphorePhases.yellow();
             else semaphorePhases.green();
         }, 2000);
-        automatic;
     }
 };
