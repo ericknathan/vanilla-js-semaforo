7e69a88b241961dafc5e5cbda886f32b7f8d94db
ericknathan <erick.capito@hotmail.com>
2021-09-01 11:12:48 -0300

    🐛 Fixed automatic function bug

--- scripts.js
+++ scripts.js
@@ -3,37 +3,39 @@ const getId = (id) => document.getElementById(id);
 const newListener = (elementId, eventName, action) => getId(elementId).addEventListener(eventName, action);
 let automatic;
 
-newListener('red', 'click', () => semaphorePhases.red())
-newListener('yellow', 'click', () => semaphorePhases.yellow())
-newListener('green', 'click', () => semaphorePhases.green())
-newListener('automatic', 'click', () => semaphorePhases.automatic())
-newListener('off', 'click', () => semaphorePhases.off())
+newListener('red', 'click', () => {
+    clearInterval(automatic);
+    semaphorePhases.red();
+});
+newListener('yellow', 'click', () => {
+    clearInterval(automatic);
+    semaphorePhases.yellow();
+});
+newListener('green', 'click', () => () => {
+    clearInterval(automatic);
+    semaphorePhases.green();
+});
+newListener('automatic', 'click', () => semaphorePhases.automatic());
+newListener('off', 'click', () => semaphorePhases.off());
 
 const semaphorePhases = {
     semaphore: getId('semaphore'),
-    red: () => {
-        clearInterval(automatic);
-        this.semaphore.src = "./public/images/vermelho.png";
-    },
-    yellow: () => {
-        clearInterval(automatic);
-        this.semaphore.src = "./public/images/amarelo.png";
-    },
-    green: () => {
-        clearInterval(automatic);
-        this.semaphore.src = "./public/images/verde.png";
-    },
+    red: () => this.semaphore.src = "./public/images/vermelho.png",
+    yellow: () => this.semaphore.src = "./public/images/amarelo.png",
+    green: () => this.semaphore.src = "./public/images/verde.png",
     off: () => {
         clearInterval(automatic);
         this.semaphore.src = "./public/images/desligado.png";
     },
     automatic: () => {
         clearInterval(automatic);
-        semaphorePhases.red();
-        automatic = setInterval(() => {
-            if(this.semaphore.src.includes("verde")) semaphorePhases.red();
-            else if(this.semaphore.src.includes("vermelho")) semaphorePhases.yellow();
+        const switchImages = () => {
+            const src = this.semaphore.src;
+            if(src.includes("verde") || src.includes("desligado")) semaphorePhases.red();
+            else if(src.includes("vermelho")) semaphorePhases.yellow();
             else semaphorePhases.green();
-        }, 2000);
+        }
+        switchImages();
+        automatic = setInterval(switchImages, 2000);
     }
 };
